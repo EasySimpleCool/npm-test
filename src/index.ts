4012209c9cbad57d182e7bfc0e211d6f0ce02985
EasySimpleCool <easysimplecool@gmail.com>
2026-07-23 15:04:20 +1000

    chore: add fontsource dependencies and import font styles in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import './tokens/variables.css';
 import './tokens/typography.css';
+import '@fontsource/space-grotesk/400.css';
+import '@fontsource/space-grotesk/700.css';
+import '@fontsource/space-mono/400.css';
+import '@fontsource/space-mono/700.css';
+import '@fontsource/ibm-plex-mono/400.css';
+import '@fontsource/ibm-plex-mono/500.css';
+import '@fontsource/ibm-plex-mono/600.css';
 
 export * from './components/Button';
 export * from './components/Input';
